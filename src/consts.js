addce220d17cd75414a7c34a61fa5be505bae29c
Christian Miranda <christianrmiranda@gmail.com>
2021-02-21 03:37:24 -0800

    add melee and some brawl characters to roster

--- src/consts.js
+++ src/consts.js
@@ -11,6 +11,30 @@ export const characters = [
   'ness',
   'captain_falcon',
   'jigglypuff',
+  'peach',
+  'bowser',
+  'ice_climbers',
+  'sheik',
+  'zelda',
+  'dr_mario',
+  'pichu',
+  'falco',
+  'marth',
+  'young_link',
+  'ganondorf',
+  'mewtwo',
+  'roy',
+  'mr_game_and_watch',
+  'meta_knight',
+  'pit',
+  'zero_suit_samus',
+  'wario',
+  'snake',
+  'ike',
+  'squirtle',
+  'ivysaur',
+  'charizard',
+  'diddy_kong',
 ];
 
 export const characterData = {
@@ -61,6 +85,102 @@ export const characterData = {
   'jigglypuff': {
     'displayName': 'Jigglypuff',
     'series': 'pokemon',
+  },
+  'peach': {
+    'displayName': 'Peach',
+    'series': 'super_mario',
+  },
+  'bowser': {
+    'displayName': 'Bowser',
+    'series': 'super_mario',
+  },
+  'ice_climbers': {
+    'displayName': 'Ice Climbers',
+    'series': 'ice_climber',
+  },
+  'sheik': {
+    'displayName': 'Sheik',
+    'series': 'legend_of_zelda',
+  },
+  'zelda': {
+    'displayName': 'Zelda',
+    'series': 'legend_of_zelda',
+  },
+  'dr_mario': {
+    'displayName': 'Dr. Mario',
+    'series': 'super_mario',
+  },
+  'pichu': {
+    'displayName': 'Pichu',
+    'series': 'pokemon',
+  },
+  'falco': {
+    'displayName': 'Falco',
+    'series': 'star_fox',
+  },
+  'marth': {
+    'displayName': 'Marth',
+    'series': 'fire_emblem',
+  },
+  'young_link': {
+    'displayName': 'Young Link',
+    'series': 'legend_of_zelda',
+  },
+  'ganondorf': {
+    'displayName': 'Ganondorf',
+    'series': 'legend_of_zelda',
+  },
+  'mewtwo': {
+    'displayName': 'Mewtwo',
+    'series': 'pokemon',
+  },
+  'roy': {
+    'displayName': 'Roy',
+    'series': 'fire_emblem',
+  },
+  'mr_game_and_watch': {
+    'displayName': 'Mr. Game & Watch',
+    'series': 'game_and_watch',
+  },
+  'meta_knight': {
+    'displayName': 'Meta Knight',
+    'series': 'kirby',
+  },
+  'pit': {
+    'displayName': 'Pit',
+    'series': 'kid_icarus',
+  },
+  'zero_suit_samus': {
+    'displayName': 'Zero Suit Samus',
+    'series': 'metroid',
+  },
+  'wario': {
+    'displayName': 'Wario',
+    'series': 'wario',
+  },
+  'snake': {
+    'displayName': 'Snake',
+    'series': 'metal_gear',
+  },
+  'ike': {
+    'displayName': 'Ike',
+    'series': 'fire_emblem',
+  },
+  'squirtle': {
+    'displayName': 'Squirtle',
+    'series': 'pokemon',
+  },
+  'ivysaur': {
+    'displayName': 'Ivysaur',
+    'series': 'pokemon',
+  },
+  'charizard': {
+    'displayName': 'Charizard',
+    'series': 'pokemon',
+  },
+  'diddy_kong': {
+    'displayName': 'Diddy Kong',
+    'series': 'donkey_kong',
   },
 };
 
